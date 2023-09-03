cb9f0997e59b0b0849fe1e11d0c4d38ba763b1b3
quillcraftsman <quill@craftsman.lol>
2023-09-03 20:49:35 +0400

    add svelte frontend project

---
+++ frontend/src/routes/Header.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+
+	import {
+        Collapse,
+        Navbar,
+        NavbarToggler,
+        NavbarBrand,
+        Nav,
+        NavItem,
+        NavLink,
+        Dropdown,
+        DropdownToggle,
+        DropdownMenu,
+        DropdownItem
+  } from 'sveltestrap';
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+
+</script>
+
+<header>
+  <Navbar color="primary" light expand="md">
+      <NavbarBrand href="/">demo.findsimilar.org</NavbarBrand>
+      <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+      <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ms-auto" navbar>
+          <NavItem>
+            <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/simple">Simple Usage</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+
+</header>
---
+++ frontend/src/routes/simple/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { Container, Form, FormGroup, Label, Button, Input } from 'sveltestrap';
+	const default_separator = ','
+	const films = 'Титаник,Матрица,Форрест Гамп,Терминатор 2,Гаттака,С широко закрытыми глазами,Основной инстинкт,Молчание ягнят,Зеленая миля,Достучаться до небес,Пятый элемент,Побег из Шоушенка,Телохранитель,Люди в черном,Бойцовский клуб,Один дома,Бетховен,Криминальное чтиво,Умница Уилл Хантинг,Маска,Знакомьтесь, Джо Блэк,День сурка,Большой Лебовски,Леон,Американский пирог,Шоу Трумана,Красотка,Адвокат дьявола'
+</script>
+
+<svelte:head>
+	<title>Simple usage</title>
+	<meta name="description" content="Simple usage example" />
+</svelte:head>
+
+<Container fluid>
+   <Form method="post">
+    <FormGroup>
+        <Label for="text">Input text to search</Label>
+        <Input id="text" name="text" placeholder="For example: Люди и глаза" />
+    </FormGroup>
+     <FormGroup>
+        <Label for="texts">Input texts to find similars through the separator (default ,)</Label>
+        <Input type="textarea" cols="40" rows="10" name="texts" id="texts" placeholder={films}/>
+      </FormGroup>
+       <FormGroup>
+        <Label for="separator">Input separator (Default {default_separator})</Label>
+        <Input id="separator" name="separator" placeholder="Separator. Default {default_separator}"  value="{default_separator}"/>
+    </FormGroup>
+    <Button type="button" color="primary">Find similar</Button>
+  </Form>
+</Container>
