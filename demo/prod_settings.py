d425780d6bb8f936ad1f67a0d2fcd1cf2bd20ece
quillcraftsman <quill@craftsman.lol>
2023-09-07 17:41:10 +0400

    add csrf trusted origins

--- demo/prod_settings.py
+++ demo/prod_settings.py
@@ -7,7 +7,7 @@ from .base_settings import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000', 'api.findsimilar.org']
 STATIC_ROOT = 'static'
 
 CORS_ALLOWED_ORIGINS = [
