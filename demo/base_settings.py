d7bc6f0ab68199f75064ebf683eaba76d48ff64f
quillcraftsman <quill@craftsman.lol>
2023-09-12 20:38:48 +0400

    add build with db

--- demo/base_settings.py
+++ demo/base_settings.py
@@ -76,18 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'demo.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- demo/prod_settings.py
+++ demo/prod_settings.py
@@ -2,10 +2,22 @@
 PROD-settings
 python manage.py runserver --settings=backend.prod_settings
 """
+import os
 from .base_settings import *
 
-
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDB'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://api.findsimilar.org', 'http://188.64.12.238:8000']
 STATIC_ROOT = 'static'
