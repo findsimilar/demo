3727728f45696e3f7c705148a3d8a8647bc9ab5f
quillcraftsman <quill@craftsman.lol>
2023-09-18 01:05:13 +0400

    add example list to API

--- Makefile
+++ Makefile
@@ -9,4 +9,5 @@ coverage:
 	coverage report --omit=demo/asgi.py,demo/wsgi.py,demo/prod_settings.py,manage.py,examples/management/* --fail-under=100
 	coverage html --omit=demo/asgi.py,demo/wsgi.py,demo/prod_settings.py,manage.py,examples/management/*
 
-
+lint:
+	pylint $(shell git ls-files '*.py')
--- examples/tests/tests_serializers.py
+++ examples/tests/tests_serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.test import APITestCase
-from examples.serializers import ExampleSerializer
+from examples.serializers import ExampleSerializer, ExampleNameSerializer
 from examples.models import Example
 
 
@@ -14,3 +14,17 @@ class TestExampleSerializer(APITestCase):
     def test_serializer(self):
         serializer = ExampleSerializer(self.example)
         self.assertEqual(serializer.data, self.example_dict)
+
+
+class TestExampleNameSerializer(APITestCase):
+    def setUp(self):
+        self.example_dict = {
+            'text': 'one two',
+            'texts': ['one', 'two']
+        }
+        self.example_name = 'some_name'
+        self.example = Example.load_from_dict('some_name', self.example_dict)
+
+    def test_serializer(self):
+        serializer = ExampleNameSerializer(self.example)
+        self.assertEqual(serializer.data, {'name': self.example_name})
